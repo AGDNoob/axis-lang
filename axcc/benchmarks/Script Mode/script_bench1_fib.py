e16f9ce1d763e133c5203876e8dd6fd28bf0ac0d
AXIS Language <axis-lang@example.com>
2026-04-23 21:45:04 +0200

    chore(axcc): remove obsolete benchmark scripts, disasm helpers, and compiled test binaries

--- axcc/benchmarks/Script Mode/script_bench1_fib.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-def fib(n):
-    if n <= 1:
-        return n
-    return fib(n - 1) + fib(n - 2)
-
-result = fib(38)
-sys.exit(result % 256)
--- axcc/disasm_bench3.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /mnt/c/Users/marlo/Projekte/AXIS/axcc
-./axis "benchmarks/Compile Mode/bench3_loops.axis" -O1 -o /tmp/b3o1
-python3 disasm_elf.py /tmp/b3o1 2>&1 | tail -n +57 | head -100
